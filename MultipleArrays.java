fd0864991853ce0092be250e530afff61dbeb3a8
KBerUX <kbearmoy@gmail.com>
2023-10-23 06:56:51 -0500

    2d array notes

--- MultipleArrays.java
+++ MultipleArrays.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MultipleArrays {
 
 //two dimensional array can be see as a dimensional of values
@@ -10,7 +13,7 @@ public class MultipleArrays {
             {11,12,13},
             {21, 22, 23, 24, 26}
     };
-}
+
 // here we have an array with 3 elements
 //    each element is an array of integers (a nested array)
 //    each nested array is a different length
@@ -31,5 +34,73 @@ public class MultipleArrays {
     //    most common ways to decalre two-dimensional array:
     int[][] myDoubleArray;
     int[] myDoubleTwoArray[];
+
+    public static void main(String[] args){
+        int[][] array2 = new int[4][4];
+        System.out.println(Arrays.toString(array2));
+        System.out.println("array2.length = " + array2.length);
+
+        for(int[] outer: array2){
+//            System.out.println(outer);
+//            prints each nested array is printed - and confirm each val is initialized to 0.
+            System.out.println(Arrays.toString(outer));
+        }
+
+        for(int i =0; i < array2.length; i++){
+            int innerArray[] = array2[i];
+            for(int j = 0; j < innerArray.length; j++){
+//                System.out.println(array2[i][j] + " ");
+
+                array2[i][j] = (i * 10) + (j + 1);
+            }
+            System.out.println();
+        }
+        System.out.println(Arrays.deepToString(array2));
+
+//        for(var outer:array2){
+//            for(var element:outer){
+//                System.out.println(element + " ");
+//                System.out.println();
+//            }
+//        }
+
+//        System.out.println(Arrays.deepToString(array2));
+
+
+//        Multiple Arrays topic:
+//        no requirement of nested arrays to be particular size
+//        need the array creation new int[]
+        array2[1] = new int[] {10, 20, 30};
+        System.out.println(Arrays.deepToString(array2));
+
+
+//        Can create an array of objects -
+        Object[] anyArray = new Object[3];
+        System.out.println(Arrays.toString(anyArray));
+        anyArray[0] = new String[] {"a", "b", "c"};
+        System.out.println(Arrays.deepToString(anyArray));
+        System.out.println();
+        anyArray[1] = new String[][]{
+                {"1", "2"},
+                {"3", "4", "5"},
+                {"6", "7", "8", "9"}
+        };
+        System.out.println(Arrays.deepToString(anyArray));
+        System.out.println();
+
+        anyArray[2] = new int[2][2][2];
+        System.out.println(Arrays.deepToString(anyArray));
+
+        System.out.println();
+        System.out.println("each array and its responding element type printed out: ");
+        for(Object element: anyArray){
+            System.out.println("Element type = " + element.getClass().getSimpleName());
+            System.out.println();
+            System.out.println("Element to String() = " + element);
+            System.out.println(Arrays.deepToString((Object[]) element));
+        }
+    }
+
+// array can be primitive, object, or any data.
 }
 
